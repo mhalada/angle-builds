d356d1dd90730f613437acaf207cf80ef7ff61b2
Michal Halada <michal.halada@maptiler.com>
2024-09-03 20:26:00 +0200

    install cmake manually

--- build-swiftshader-docker.sh
+++ build-swiftshader-docker.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-apt-get -y install cmake
+apt-get -y install build-essential
 
-./build-swiftshader.sh
+curl -L -o /tmp/cmake.sh https://github.com/Kitware/CMake/releases/download/v3.30.2/cmake-3.30.2-linux-aarch64.sh
+chmod +x /tmp/cmake.sh
+/tmp/cmake.sh --skip-license --prefix=/usr
+rm /tmp/cmake.sh
+
+/data/build-swiftshader.sh
