851fd3bc07f5b96dc6f7e512a20ed24e81d13cda
Michal Halada <michal.halada@maptiler.com>
2024-09-04 17:00:06 +0200

    add zlib1g-dev to swiftshader docker build

--- build-swiftshader-docker.sh
+++ build-swiftshader-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt-get update && apt-get -y install build-essential curl libx11-dev libxext-dev python3
+apt-get update && apt-get -y install build-essential curl libx11-dev libxext-dev python3 zlib1g-dev
 
 curl -L -o /tmp/cmake.sh https://github.com/Kitware/CMake/releases/download/v3.30.2/cmake-3.30.2-linux-aarch64.sh
 chmod +x /tmp/cmake.sh
