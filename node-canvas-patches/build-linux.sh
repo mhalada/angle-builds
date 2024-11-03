3177f323340367d119cb9483a81f6adac03e337e
Michal Halada <michal.halada@maptiler.com>
2024-11-03 22:16:56 +0100

    fix package removal

--- node-canvas-patches/build-linux.sh
+++ node-canvas-patches/build-linux.sh
@@ -4,6 +4,6 @@ node-gyp rebuild -j 2
 . prebuild/Linux/bundle.sh
 cp -a build/. output/
 
-apt-get remove packagename libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
+apt-get remove libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
 apt-get autoremove
 npm test
