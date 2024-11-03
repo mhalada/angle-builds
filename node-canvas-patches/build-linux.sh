eb91aa721fdb04381cb18ceb0096809900f00246
Michal Halada <michal.halada@maptiler.com>
2024-11-03 22:20:41 +0100

    fix output copying

--- node-canvas-patches/build-linux.sh
+++ node-canvas-patches/build-linux.sh
@@ -2,7 +2,7 @@
 
 node-gyp rebuild -j 2
 . prebuild/Linux/bundle.sh
-cp -a build/. output/
+cp -a /data/build/. /data/output/
 
 apt-get remove libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
 apt-get autoremove
