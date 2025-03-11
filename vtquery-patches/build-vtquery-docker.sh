928db00e93245136bf55646413cb8be2cbebfd22
Michal Halada <michal.halada@maptiler.com>
2025-03-11 16:34:52 +0100

    use ubuntu 22.04 for docker arm64 builds

--- vtquery-patches/build-vtquery-docker.sh
+++ vtquery-patches/build-vtquery-docker.sh
@@ -2,6 +2,14 @@
 
 apt-get update && apt-get -y install build-essential curl
 
+# Ubuntu 20.04
+#   - /lib/aarch64-linux-gnu/libstdc++.so.6: version GLIBCXX_3.4.29' not found
+#   - /lib/aarch64-linux-gnu/libc.so.6: version `GLIBC_2.32' not found
+# export DEBIAN_FRONTEND=noninteractive
+# apt-get -y install build-essential curl software-properties-common
+# add-apt-repository ppa:ubuntu-toolchain-r/test -y
+# apt-get update && apt-get install gcc-11 g++-11 -y
+
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
 apt-get install nodejs -y
 
