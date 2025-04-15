d84f25a1ceb6ded480be3bb36ce0e74e563fb9b5
Michal Halada <michal.halada@maptiler.com>
2025-04-15 13:28:49 +0200

    add missing changes for canvas arm64 build

--- node-canvas-patches/bundle.sh
+++ node-canvas-patches/bundle.sh
@@ -1,5 +1,17 @@
-copies=$(/data/pax-utils/lddtree.sh -l build/Release/canvas.node | sed -r -e '/canvas.node$/d');
+set -ex
+
+apt install -y patchelf pax-utils
+
+copies=$(lddtree -l build/Release/canvas.node | sed -r -e '/^\/lib/d' -e '/canvas.node$/d');
+
+# remove the big artifacts we will not use.
+rm -r build/Release/.deps
+rm -r build/Release/obj.target
 
 for so in $copies; do
   cp $so build/Release
+  # Set the run_path for all dependencies.
+  patchelf --set-rpath '$ORIGIN' build/Release/$(basename $so)
 done;
+
+find "./build/Release" -type f -name "*.so*" -exec strip --strip-unneeded {} \;
