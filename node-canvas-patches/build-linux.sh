ed560630401c7aad8eebe90312f20aa15e2ade2f
Michal Halada <michal.halada@maptiler.com>
2025-04-18 11:01:27 +0200

    fix uninstall sequence

--- node-canvas-patches/build-linux.sh
+++ node-canvas-patches/build-linux.sh
@@ -6,8 +6,9 @@ cp -a /data/build/. /data/output/
 
 cd /root/harfbuzz-* && make uninstall
 cd /root/cairo-* && make uninstall
-cd /root/pango-* && make uninstall
+cd /root/pango-* && cd _build && ninja uninstall
 cd /root/libpng-* && make uninstall
-cd /root/libjpeg-* && make uninstall
+cd /root/libjpeg-* && cd b && make uninstall
 cd /root/giflib-* && make uninstall
+
 cd /data && npm test
