8a225dd1a11036dafae52436cba8c8c7e2630f88
Michal Halada <michal.halada@maptiler.com>
2025-04-18 10:18:32 +0200

    use node-canvas prebuilt docker image

--- node-canvas-patches/build-linux.sh
+++ node-canvas-patches/build-linux.sh
@@ -4,13 +4,10 @@ node-gyp rebuild -j 2
 . prebuild/Linux/bundle.sh
 cp -a /data/build/. /data/output/
 
-apt-get -y remove libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
-apt-get -y autoremove
-npm test
-# cd /root/harfbuzz-* && make uninstall
-# cd /root/cairo-* && make uninstall
-# cd /root/pango-* && make uninstall
-# cd /root/libpng-* && make uninstall
-# cd /root/libjpeg-* && make uninstall
-# cd /root/giflib-* && make uninstall
-# cd /data && npm test
+cd /root/harfbuzz-* && make uninstall
+cd /root/cairo-* && make uninstall
+cd /root/pango-* && make uninstall
+cd /root/libpng-* && make uninstall
+cd /root/libjpeg-* && make uninstall
+cd /root/giflib-* && make uninstall
+cd /data && npm test
